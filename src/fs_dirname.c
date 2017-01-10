50da543170c4cf30d9b1244bf8b89dac80c1ebfc
Ian Wordes <iwordes@student.42.us.org>
2017-01-10 13:21:11 -0800

    Fix dirname

--- src/fs_dirname.c
+++ src/fs_dirname.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 19:56:39 by iwordes           #+#    #+#             */
-/*   Updated: 2017/01/09 20:14:34 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/01/10 13:20:43 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ char	*fs_dirname(const char *path)
 
 	NULL_GUARD(path);
 	i = fs__strlen(path);
-	while (path[i] == '/')
+	if (i > 0)
+		i -= 1;
+	while (i > 0 && path[i] == '/')
 		i -= 1;
 	while (i > 0 && path[i] != '/')
 		i -= 1;
-	while (path[i] == '/')
+	while (i > 1 && path[i] == '/')
 		i -= 1;
-	return (fs__strsub(path, 0, i));
+	return (fs__strsub(path, 0, i + 1));
 }
