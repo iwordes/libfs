be53b51530aaa63a03edb9ad44a6fa5285f4f132
Ian Wordes <iwordes@student.42.us.org>
2017-01-13 17:56:51 -0800

    Fix issue with fs_join where final string may have one junk byte

--- src/fs_join.c
+++ src/fs_join.c
@@ -6,7 +6,7 @@
 /*   By: iwordes <iwordes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/04 09:30:36 by iwordes           #+#    #+#             */
-/*   Updated: 2017/01/13 16:47:20 by iwordes          ###   ########.fr       */
+/*   Updated: 2017/01/13 17:56:15 by iwordes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,6 @@ char	*fs_join(const char *path1, const char *path2)
 	if (path1[l1 - 1] != '/')
 		l1 += 1;
 	fs__memcpy(path + l1, path2, l2);
-	path[l1 + l2 + 1] = 0;
+	path[l1 + l2] = 0;
 	return (path);
 }
